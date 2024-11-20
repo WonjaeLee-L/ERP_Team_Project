f0514e78594a2c255977b1946e082eeffdd3907f
WonajaeLee-L <dldnjswo@gmail.com>
2024-11-20 11:09:15 +0900

    project_main

---
+++ src/main/java/com/example/practice/repository/IF_MemberDao.java
@@ -0,0 +1,15 @@
+package com.example.practice.repository;
+
+
+import com.example.practice.vo.MemberVO;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface IF_MemberDao {
+
+	public void insertOne(MemberVO membervo)throws Exception;
+	public MemberVO selectOne(String id) throws Exception;
+
+}
